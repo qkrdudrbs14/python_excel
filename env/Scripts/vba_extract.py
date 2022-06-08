b387887ad120917b00b204718c81432068faf2f9
pyg <parkpark14@naver.com>
2022-06-08 16:45:39 +0900

    first commit

---
+++ env/Scripts/vba_extract.py
@@ -0,0 +1,4 @@
+#!F:\pyg\pyg\react\python_excel\env\Scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'XlsxWriter==3.0.3','vba_extract.py'
+__requires__ = 'XlsxWriter==3.0.3'
+__import__('pkg_resources').run_script('XlsxWriter==3.0.3', 'vba_extract.py')
